7a7676790267cdcbddc099195eedaf3793884a25
Almoe099 <almoe9@LAPTOP-I8VP6L2K.localdomain>
2023-11-21 16:59:55 -0500

    added w4d5

---
+++ w4d5/practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd_project.rb
@@ -0,0 +1,27 @@
+class Array
+
+    def uniq
+        return self if self.length == 0
+        hash = Hash.new(0)
+        self.each {|el| hash[el] += 1}
+        hash.keys
+    end
+
+    def two_sum
+        sum = []
+    
+        (0...self.length).each do |i|
+          ((i + 1)...self.length).each do |j|
+            if self[i].is_a?(Numeric) && self[j].is_a?(Numeric) && self[i] + self[j] == 0
+              sum << [i, j]
+            end
+          end
+        end
+    
+        sum.sort_by { |a, b| [a, b] }
+    end
+
+    
+
+
+end
---
+++ w4d5/practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_project_spec.rb
@@ -0,0 +1,80 @@
+require 'rspec'
+require "tdd_project"
+
+describe Array do
+    describe '#uniq' do
+      context 'when array contains duplicates' do
+        it 'removes duplicates and keeps original order' do
+          expect([1, 2, 1, 3, 3].uniq).to eq([1, 2, 3])
+        end
+      end
+  
+      context 'when array has no duplicates' do
+        it 'returns the array as is' do
+          expect([1, 2, 3].uniq).to eq([1, 2, 3])
+        end
+      end
+  
+      context 'when array is empty' do
+        it 'returns an empty array' do
+          expect([].uniq).to eq([])
+        end
+      end
+  
+      context 'when array contains nil values' do
+        it 'treats nil as a unique element' do
+          expect([nil, nil, 1, 2].uniq).to eq([nil, 1, 2])
+        end
+      end
+  
+      context 'when array contains complex objects' do
+        it 'compares objects for uniqueness' do
+          obj1 = Object.new
+          obj2 = Object.new
+          expect([obj1, obj2, obj1].uniq).to eq([obj1, obj2])
+        end
+      end
+    end
+
+    describe '#two_sum' do
+      context 'when no pairs sum to zero' do
+        it 'returns an empty array' do
+          expect([1, 2, 3].two_sum).to eq([])
+        end
+      end
+
+      context 'when pairs sum to zero' do
+        it 'returns all pairs with smaller indices first' do
+          expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+        end
+      end
+  
+      context 'when array contains zero' do
+        it 'handles zero correctly' do
+          expect([-1, 0, 1, 2].two_sum).to eq([[0, 2]])
+        end
+      end
+  
+      context 'when array is empty' do 
+        it 'returns an empty array' do
+          expect([].two_sum).to eq([])
+        end
+      end
+    end
+
+    describe "#my_transpose" do 
+      context "when all arrays share the same length" do 
+        it "returns a maxtrix" do 
+          expect([].my_transpose).to eq([])
+        end
+      end
+  
+    end
+
+
+
+
+
+
+
+end
